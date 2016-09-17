b7110f3313e5544d745972c78d43a0adb9856a54
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2016-09-17 18:06:35 -0500

    Fixed punctuation ending word.

--- countWord.pl
+++ countWord.pl
@@ -4,35 +4,35 @@
 use LWP::Simple;
 use HTML::Strip;
 
-my $content = get("http://www.cs.memphis.edu/~vrus/teaching/ir-websearch/");
-#print($content);
-die "Couldn't get content!" unless defined $content;
-my $hs = HTML::Strip->new();
-my $page_text = $hs->parse($content);
-#print $page_text;
+$content = get("http://www.cs.memphis.edu/~vrus/teaching/ir-websearch/");
 
-foreach my $line (split /\n/, $page_text) {
-  #print $line, "\n";
+die "Couldn't get content!" unless defined $content;
+
+$hs = HTML::Strip->new();
+$page_text = $hs->parse($content);
+
+$wordCount = 0;
+foreach $line (split /\n/, $page_text) {
   @words = split(/[\s\t]+/, $line);
   $w = 0;
   while($w < @words) {
-    my @letters = split(/\s*/, $words[$w]);
-    if ($letters[@letters - 1] eq "." || $letters[@letters - 1] eq ","
-        || $letters[@letters - 1] eq ":" || $letters[@letters - 1] eq "}"
-        || $letters[@letters - 1] eq ")" || $letters[@letters - 1] eq "("
-        || $letters[@letters - 1] eq "!") {
-      # print substr($words[$w], 0, @words-2), "\n"; #note: for testing
-      $wordFrequency{$words[$w]}++;
-      # $wordFrequency{substr($words[$w], 0, @words)}++;
-    }
+      if($words[$w] =~ /[[:punct:]]$/) { # if the word contains a punctuation at the end, only count the original word without punctuation
+        #print(substr($words[$w], 0, @words[$w]-1), "\n"); # print the punctuated word
+        #if (@words[$w] > 1) { # If the word is just a space, do not include it
+          $wordFrequency{substr($words[$w], 0, @words[$w]-1)}++;
+          $wordCount++;
+        #}
+      }
+      else {
+        #print $words[$w]; # print the words that don't contain punctuation
+        $wordFrequency{$words[$w]}++;
+        $wordCount++;
+      }
     $w++;
-    $wordFrequency{$words[$w]}++;
   }
-  #print("\n");
-  $wordCount += $w;
 }
 
-print("\nTotal number of words: ", $wordCount, "\n");
+#print("\nTotal number of words: ", $wordCount, "\n");
 
 foreach $key (sort keys %wordFrequency) {
   print "\"$key\" appeared $wordFrequency{$key} times\n";
