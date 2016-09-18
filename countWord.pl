04537c1f4378769888a815775bf331f0864619a6
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2016-09-18 16:04:27 -0500

    Disregard whitespaces when counting words

--- countWord.pl
+++ countWord.pl
@@ -16,23 +16,26 @@ foreach $line (split /\n/, $page_text) {
   @words = split(/[\s\t]+/, $line);
   $w = 0;
   while($w < @words) {
+    if ($words[$w] !~ /^\s*$/) { # if the string is not only consist of spaces.
       if($words[$w] =~ /[[:punct:]]$/) { # if the word contains a punctuation at the end, only count the original word without punctuation
         #print(substr($words[$w], 0, @words[$w]-1), "\n"); # print the punctuated word
-        #if (@words[$w] > 1) { # If the word is just a space, do not include it
+        $temp = substr($words[$w], 0, @words[$w]-1); # # if the string is a punctuation, then getting rid of it will leave us with whitespace
+        if ($temp !~ /^\s*$/) { # if "temp" is not only whitespaces
           $wordFrequency{substr($words[$w], 0, @words[$w]-1)}++;
           $wordCount++;
-        #}
+        }
       }
       else {
         #print $words[$w]; # print the words that don't contain punctuation
         $wordFrequency{$words[$w]}++;
         $wordCount++;
       }
+    }
     $w++;
   }
 }
 
-#print("\nTotal number of words: ", $wordCount, "\n");
+print("\nTotal number of words: ", $wordCount, "\n");
 
 foreach $key (sort keys %wordFrequency) {
   print "\"$key\" appeared $wordFrequency{$key} times\n";
