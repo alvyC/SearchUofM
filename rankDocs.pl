bfc9497680e9d1de59b852328dbeca597235c866
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2016-11-15 21:39:24 -0600

    rank docs: compute document vector length and store the related links

--- rankDocs.pl
+++ rankDocs.pl
@@ -1,14 +1,29 @@
 #!/opt/local/bin/perl
 
-use strict;
-use warnings;
+#use strict;
+#use warnings;
+
+use LWP::Simple;
+use HTML::Strip;
+use HTML::LinkExtor;
+use WWW::Mechanize;
+use Lingua::Stem::En;
+use CGI;
 
 my $totalNoDocs = 8393;
 my $processedFileLocation = "./processed_documents/";
 my $fileExtension = ".txt";
 my %invertedIndex;
 my %idf;
-my @queryWords;
+my %stopWordHash;
+my $query = "Muktadir Chowdhury Computer Science";
+my %docVectorLength;
+my @outputLinks;
+
+&createInvertedIndex;
+#&computeIdf;
+#&printInvertedIndex;
+&computeDocumentVectorLength($query);
 
 sub createInvertedIndex {
   my $documentNo = 1;
@@ -38,10 +53,10 @@ sub createInvertedIndex {
 sub printInvertedIndex {
   open(OUTFILE, ">inverted-index.txt");
   print OUTFILE ("   Word (idf) ----> Document No | Term Frequency\n");
-  print OUTFILE ("----------------------------------------------\n");
+  print OUTFILE ("----------------------------------------------------\n");
   foreach my $word (sort keys %invertedIndex) {
     print OUTFILE ($word, " (");
-    print OUTFILE ($idf{$word}, ") ---> ");
+    #print OUTFILE ($idf{$word}, ") ---> ");
     foreach my $doc (keys %{$invertedIndex{$word}}) {
       print OUTFILE ($doc, "|", $invertedIndex{$word}{$doc}, "; ");
     }
@@ -49,14 +64,6 @@ sub printInvertedIndex {
   }
 }
 
-# df = keys %invertedIndex;
-# tf = keys %invertedIndex{$word}
-
-# Let N be the total number of Documents;
-# For each token, T, in V:
-#       Determine the total number of documents, m,
-#           in which T occurs
-#       Set the IDF for T to log(N/m);
 sub computeIdf {
   foreach my $word (sort keys %invertedIndex) {
     my $df = keys %invertedIndex;
@@ -70,6 +77,82 @@ sub computeIdf {
   }
 }
 
-&createInvertedIndex;
-&computeIdf;
-&printInvertedIndex;
+# sub initStopWordHash {
+#   my $stopWords = get("http://www.cs.memphis.edu/~vrus/teaching/ir-websearch/papers/english.stopwords.txt") || die "Cannot get stopwordSite" ;
+#   my @stopWordList = split("\n", $stopWords);
+#   foreach my $stopWord (@stopWordList) {
+#     $stopWordHash{$stopWord} = 1;
+#   }
+# }
+
+# sub removeStopWordsAndStem {
+#   my ($line) = @_;
+#   my @words = split(" ", $line);
+
+#   my $stemmed_words = Lingua::Stem::En::stem({-words => \@words,
+#                                               -locale => 'en',
+#                                               -exceptions => \%exceptions});
+#   my $processedLine = "";
+#   foreach my $word(@$stemmed_words) {
+#     if ($stopWordHash{$word} == 0) {
+#       $processedLine = $processedLine . " " . $word;
+#     }
+#   }
+
+#   return $processedLine;
+# }
+
+# sub processQuery {
+#   my ($line) = @_;
+
+#   &initStopWordHash;
+
+#   $line =~ s/((?<=[^a-zA-Z0-9])(?:https?\:\/\/|[a-zA-Z0-9]{1,}\.{1}|\b)(?:\w{1,}\.{1}){1,5}(?:com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil|iq|io|ac|ly|sm){1}(?:\/[a-zA-Z0-9]{1,})*)//g; # Remove HTML/HTTP or any kind of URL type lines.
+#   $line =~ s/[[:punct:]]//g; # Remove punctuations
+#   $line =~ s/\d//g;          # Remove digits
+#   $line =~ s/^\s+//;         # Remove leading whitespaces
+#   $line = lc $line;          # Convert uppercases to lowercases
+
+#   $line = &removeStopWordsAndStem($line); # Remove stopwords and do stemming
+
+#   return $line;
+# }
+
+sub computeDocumentVectorLength {
+  my ($query) = @_;
+  my @queryWords = split(" ", $query);
+
+  my $i = 0;
+  foreach my $word (sort keys %invertedIndex) {
+    if (lc($queryWords[$i]) eq $word) {
+      #print $word, ": \n";
+      my $df = keys %{$invertedIndex{$word}};
+      my $idf = log($totalNoDocs/$df);
+      #print "df = ", $df, "\n";
+      foreach my $doc (keys %{$invertedIndex{$word}}) {
+        #print $doc, " | ";
+        my $tf = $invertedIndex{$word}{$doc};
+        #print $tf, ", ";
+        $docVectorLength{$doc} += ($tf * $idf)
+      }
+      #print "\n";
+      $i++;
+    }
+
+    if ($i == @queryWords) {
+      last;
+    }
+  }
+
+  foreach my $doc (sort keys %docVectorLength) {
+    $docVectorLength{$doc} = sqrt($docVectorLength{$doc});
+    #print $doc, ": " ,$docVectorLength{$doc}, ", ";
+    my @array;
+    if (open(INFILE, $processedFileLocation . $doc) || die ("Can't open ", $doc, " for reading.")) {
+      @array = <INFILE>;
+    }
+    #print $array[0], "\n";
+    push(@outputLinks, $array[0]);
+  }
+  #print "\n";
+}
